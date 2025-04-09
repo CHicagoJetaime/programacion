5d7570456cc13cc37f8c63b20295cf60dcfb77b6
CHicagoJetaime <jdmg280608@gmail.com>
2025-04-08 23:39:34 -0600

    Create r.php

---
+++ guardado/r.php
@@ -0,0 +1,27 @@
+<?php
+include '../conexion/r.php';
+$a = $_POST['a'];
+$b = $_POST['b'];
+$c = $_POST['c'];
+$d = $_POST['d'];
+$e = $_POST['e'];
+$f = $_POST['e'];
+$g = $_POST['g'];
+ 
+$ingresar = "INSERT into registrado values('$a', '$b', '$c', '$d', '$e', '$f', '$g')"; 
+$query = mysqli_query($conectar, $ingresar); 
+
+if($query){
+    echo "<script> alert('Ingresado Correctamente!!');
+        location.href = '../proyecto/r.html';
+        </script>";
+        }
+        else{
+            echo "<script> alert('Error!!');
+            location.href = '../proyecto/r.html';
+            </script>";
+        }   
+        ?>
+
+
+
