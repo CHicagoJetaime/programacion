3ffdd0302312ff9b9ecbe4c3068cb1eec97506e1
CHicagoJetaime <jdmg280608@gmail.com>
2025-05-13 16:27:59 -0600

    Update conexion.php

--- bs/conexion.php
+++ bs/conexion.php
@@ -1,15 +1,8 @@
 <?php
-$servername = "sql311.infinityfree.com";  // Reemplaza con tu servidor
-$username = "if0_38704752";  // Tu usuario de InfinityFree
-$password = "ZJKopSPvSZe";  // La contraseña
-$dbname = "if0_38704752_XXX";  // Nombre de la base de datos
-
-// Crear la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-echo "Conexión exitosa";
+$server = "sql311.infinityfree.com";
+$username = "if0_38704752";
+$password = "ZJKopSPvSZe";
+$database = "if0_38704752_XXX";
+$conectar = mysqli_connect($server, $username, $password, $database);
 ?>
+
