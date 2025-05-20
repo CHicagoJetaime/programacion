1a22a15fe13e78c37f71583bbe0c44943394bd3e
CHicagoJetaime <jdmg280608@gmail.com>
2025-05-20 15:15:33 -0600

    Delete bs directory

--- bs/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "sql311.infinityfree.com";
-$username = "if0_38704752";
-$password = "ZJKopSPvSZe";
-$database = "if0_38704752_XXX";
-$conectar = mysqli_connect($server, $username, $password, $database);
-?>
-
